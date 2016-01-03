986c73a3792276ed5eeab9373f4132ed153d7596
shiro <shironecko.mail@gmail.com>
2016-01-03 15:35:48 +0200

    inplement platform file functions and get resource converter functional

--- source/linux32_platform.cpp
+++ source/linux32_platform.cpp
@@ -18,192 +18,147 @@ bool PlatformWriteFile(const char* path, void* memory, u32 bytesToWrite);
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 
 u64 PlatformGetFileSize(const char* path)
 {
-  return 0;
+  struct stat fileStats;
+  i32 statResult = stat(path, &fileStats);
+
+  if (statResult != 0)
+    return 0;
+
+  return fileStats.st_size;
 }
 
 u32 PlatformLoadFile(const char* path, void* memory, u32 memorySize)
 {
-  return 0;
+  i32 file = open(path, O_RDONLY);
+
+  if (file == -1)
+    return 0;
+
+  ssize_t readResult = read(file, memory, memorySize);
+  close(file);
+
+  return readResult == -1 ? 0 : readResult;
 }
 
 bool PlatformWriteFile(const char* path, void* memory, u32 bytesToWrite)
 {
-  return false;
+  // TODO: handle directory creation
+
+  i32 file = open(path, O_WRONLY | O_CREAT | O_TRUNC);
+  if (file == -1)
+    return false;
+
+  ssize_t writeResult = write(file, memory, bytesToWrite);
+  close(file);
+
+  return writeResult == bytesToWrite;
 }
 
-struct
+void Linux32SetupRenderingBuffers(
+    RenderTarget* renderTarget, 
+    u32 width, 
+    u32 height, 
+    void* buffersMemory, 
+    u32 buffersMemorySize)
 {
-  bool            shouldRun           = true;
-  u32             windowWidth         = 640;
-  u32             windowHeight        = 480;
+  u8* memory = (u8*)buffersMemory;
+  renderTarget->texture = (Texture*)memory;
+  memory += sizeof(Texture) + width * height * sizeof(Color32);
+  renderTarget->texture->width = width;
+  renderTarget->texture->height = height;
 
-  void*           memory              = nullptr;
-  u32             memorySize          = 0;
-  RenderTarget    renderBuffer        {};
-
-} g_platformData;
+  renderTarget->zBuffer = (float*)memory;
+  memory += sizeof(float) * width * height;
+}
 
 int main(int argc, char** argv)
 {
-  Display *display;
-  int screen;
-  Window window;
-  GC graphicsContext;
-  
-  const u32 windowWidth = 1280;
-  const u32 windowHeight = 720;
+  Display* display = XOpenDisplay((char *)0);
+  i32 screen = DefaultScreen(display);
+  u32 black = BlackPixel(display,screen);
+  u32 white = WhitePixel(display, screen);
 
-  /* get the colors black and white (see section for details) */
-  unsigned long black,white;
+  u32 windowWidth = 640;
+  u32 windowHeight = 480;
 
-  /* use the information from the environment variable DISPLAY 
-     to create the X connection:
-  */  
-  display = XOpenDisplay((char *)0);
-  screen = DefaultScreen(display);
-  black = BlackPixel(display,screen), /* get color black */
-  white = WhitePixel(display, screen);  /* get color white */
-
-  /* once the display is initialized, create the window.
-     This window will be have be 200 pixels across and 300 down.
-     It will have the foreground white and background black
-  */
-  window = XCreateSimpleWindow(display, DefaultRootWindow(display),0,0, windowWidth, windowHeight, 5, white, black);
-
-  /* here is where some properties of the window can be set.
-     The third and fourth items indicate the name which appears
-     at the top of the window and the name of the minimized window
-     respectively.
-  */
+  Window window = XCreateSimpleWindow(display, DefaultRootWindow(display),0,0, windowWidth, windowHeight, 5, white, black);
   XSetStandardProperties(display, window,"Software Renderer","Hoi!",None,NULL,0,NULL);
+  XSelectInput(display, window, ButtonPressMask | KeyPressMask);
 
-  /* this routine determines which types of input are allowed in
-     the input.  see the appropriate section for details...
-  */
-  XSelectInput(display, window, ExposureMask|ButtonPressMask|KeyPressMask);
-
-  /* create the Graphics Context */
-  graphicsContext = XCreateGC(display, window, 0,0);        
-
-  /* here is another routine to set the foreground and background
-     colors _currently_ in use in the window.
-  */
+  GC graphicsContext = XCreateGC(display, window, 0,0);        
   XSetBackground(display,graphicsContext,white);
   XSetForeground(display,graphicsContext,black);
 
-  /* clear the window and bring it on top of the other windows */
   XClearWindow(display, window);
   XMapRaised(display, window);
 
-  // GAME LOOP
-  XEvent event;   /* the XEvent declaration !!! */
-  KeySym key;   /* a dealie-bob to handle KeyPress Events */  
-  char text[255];   /* a char buffer for KeyPress Events */
+   //*****ALLOCATING MEMORY*****//
+  u32 platformMemorySize = 128 * Mb;
+  void* platformMemory = malloc(platformMemorySize);
+  assert(platformMemory);
 
-  /* look for events forever... */
-  while(1) 
+  u32 gameMemorySize = 512 * Mb;
+  void* gameMemory = malloc(gameMemorySize);
+  assert(gameMemory);
+
+  RenderTarget renderBuffer{};
+  Linux32SetupRenderingBuffers(
+      &renderBuffer, 
+      windowWidth, 
+      windowHeight, 
+      platformMemory,
+      platformMemorySize);
+
+  XEvent event;
+  KeySym key;
+  char text[255];
+  Input input{};
+
+  GameInitialize(gameMemory, gameMemorySize);
+
+  bool continueRunning = true;
+  do
   {    
-    /* get the next event and stuff it into our event variable.
-       Note:  only events we set the mask for are detected!
-    */
-    XNextEvent(display, &event);
+    while (XPending(display))
+    {
+      XNextEvent(display, &event);
+      if (event.type == KeyPress && XLookupString(&event.xkey, text, 255, &key, 0) == 1) 
+      {
+        if (text[0] == 'q') 
+          continueRunning = false;
+
+        printf("You pressed the %c key!\n",text[0]);
+      }
+
+      if (event.type == ButtonPress) 
+        printf("You pressed a button at (%i,%i)\n", event.xbutton.x,event.xbutton.y);
+    }
   
-    if (event.type == Expose && event.xexpose.count == 0) 
-    {
-      /* the window was exposed redraw it! */
-      //redraw();
-    }
+    // GAME UPDATE
+    bool gameWantsToContinue = GameUpdate(
+        0.1f,
+        gameMemory,
+        gameMemorySize,
+        &renderBuffer,
+        &input);
 
-    if (event.type == KeyPress && XLookupString(&event.xkey, text, 255, &key, 0) == 1) 
-    {
-      /* use the XLookupString routine to convert the invent
-         KeyPress data into regular text.  Weird but necessary...
-      */
-      if (text[0] == 'q') 
-        break;
-
-      printf("You pressed the %c key!\n",text[0]);
-    }
-    if (event.type == ButtonPress) 
-    {
-      /* tell where the mouse Button was Pressed */
-      printf("You pressed a button at (%i,%i)\n", event.xbutton.x,event.xbutton.y);
-    }
-  }
+    continueRunning &= gameWantsToContinue;
+  } while (continueRunning);
 
   XFreeGC(display, graphicsContext);
   XDestroyWindow(display,window);
   XCloseDisplay(display); 
-
   return 0;
 }
-
-/* int CALLBACK WinMain( */
-/*   HINSTANCE instance, */
-/*   HINSTANCE /1* prevInstance *1/, */
-/*   char*     /1* cmdLine *1/, */
-/*   int       /1* cmdShow *1/) */
-/* { */
-   //*****CREATING A WINDOW*****//
-/*   WNDCLASSEX wndClass {}; */
-/*   wndClass.cbSize = sizeof(wndClass); */
-/*   wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC; */
-/*   wndClass.lpfnWndProc = Win32WindowProc; */
-/*   wndClass.hInstance = instance; */
-/*   wndClass.lpszClassName = "Software Renderer Window Class Name"; */
-
-/*   RegisterClassEx(&wndClass); */
-
-/*   HWND window = CreateWindowEx( */
-/*     0, */
-/*     wndClass.lpszClassName, */
-/*     "Software Renderer", */
-/*     WS_OVERLAPPEDWINDOW | WS_VISIBLE, */
-/*     CW_USEDEFAULT, */
-/*     CW_USEDEFAULT, */
-/*     g_platformData.windowWidth, */
-/*     g_platformData.windowHeight, */
-/*     0, */
-/*     0, */
-/*     instance, */
-/*     nullptr */
-/*   ); */
-
-   //*****ALLOCATING MEMORY*****//
-/*   g_platformData.memorySize = 128 * Mb; */
-/*   g_platformData.memory = VirtualAlloc( */
-/*     nullptr, */
-/*     g_platformData.memorySize, */
-/*     MEM_RESERVE | MEM_COMMIT, */
-/*     PAGE_READWRITE */
-/*   ); */
-/*   assert(g_platformData.memory); */
-
-/*   u32   gameMemorySize = 512 * Mb; */
-/*   void* gameMemory = VirtualAlloc( */
-/*     nullptr, */
-/*     gameMemorySize, */
-/*     MEM_RESERVE | MEM_COMMIT, */
-/*     PAGE_READWRITE */
-/*   ); */
-/*   assert(gameMemory); */
-
-   //*****MISC SETUP*****//
-/*   Win32SetupRenderingBuffers(g_platformData.windowWidth, g_platformData.windowHeight); */
-
-/*   HDC windowDC = GetDC(window); */
-/*   MSG message {}; */
-/*   Input input {}; */
-
-/*   LARGE_INTEGER lastFrameTime; */
-/*   LARGE_INTEGER queryFrequency; */
-/*   QueryPerformanceCounter(&lastFrameTime); */
-/*   QueryPerformanceFrequency(&queryFrequency); */
-
 /*   GameInitialize(gameMemory, gameMemorySize); */
 
    //*****RENDERING LOOP*****//
