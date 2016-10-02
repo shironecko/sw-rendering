456affe2ff8134aa70692cee5f1dafc2abb30025
shiro <shironecko.mail@gmail.com>
2016-10-02 14:53:27 +0300

    add assets symlinking to the build script

--- misc/build.bat
+++ misc/build.bat
@@ -11,3 +11,11 @@ cl /nologo .\source\win32_platform.cpp ^
     -Fo.\build\ -Fd.\build\game.pdb -Fe.\build\game.exe ^
     /W4 /wd4100 /wd4201 /wd4505 /MT /Od /Oi /Zi ^
     /link /subsystem:windows user32.lib gdi32.lib
+
+REM create symlink of assets folder
+if not exist .\build\assets (
+    mklink /D .\build\assets ..\assets
+    if ERRORLEVEL 1 (
+        echo [WARNING] Assets folder symlink creation failed, probably not running this as admin...
+    )
+)
